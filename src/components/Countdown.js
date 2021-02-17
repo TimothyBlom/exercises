4608962e3f1cf3d3011d1cc251df620dd1fc09be
kracok <kracok@gmail.com>
2021-02-17 23:36:51 +0100

    added timer component

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,18 +1,18 @@
 import React, { useState } from 'react';
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
+import CountdownComponent from "./CountdownComponent";
 
 const Countdown = () => {
     const [selectedDate, setSelectedDate] = useState(null);
-    
-    const calculateTimeLeft = () => {
-        let year = new Date().getFullYear();
-    }
 
     return (
         <div class='countdown'>
 
-            <p className="pageHeaderText">Countdown Timer</p>
+            <p className="pageHeaderText">
+                For this exercise I made an countdown timer for a specific date (very important dates!). 
+                I also imported the Date Picker component
+            </p>
 
             <div className="datePickerContainer">
                 <p>Date Picker Component</p>
@@ -25,7 +25,17 @@ const Countdown = () => {
                     scrollableMonthYearDropdown
                     isClearable
                 />
-            </div>              
+            </div>
+
+            <CountdownComponent 
+                timerDate={"June 11, 2021"}
+                timerDescription={"The release date for the video game Ratchet & Clank Rift Apart"}
+            />
+
+            <CountdownComponent 
+                timerDate={"May 14, 2021"}
+                timerDescription={"The release date for the video game Mass Effect Legendary Edition"}
+            />
 
         </div>
     )
