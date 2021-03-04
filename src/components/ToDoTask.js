c654338c6f7166802c521072ab6058b8d6286175
kracok <kracok@gmail.com>
2021-03-04 12:45:02 +0100

    updated todolist

--- src/components/ToDoTask.js
+++ src/components/ToDoTask.js
@@ -10,13 +10,13 @@ const Task = (props) => {
 
     return (
         <div className="taskItem">
-            {/* <img className="taskbarSideL" src={sliderImg1} /> */}
 
             <img className="taskPlatform" src={sliderImg1} />
-            <p className="taskName">{props.taskText}</p>
+            <img className="taskLogo" src={sliderImg1} />
+            {/* <p className="taskName">{props.taskText}</p> */}
+            <input type="text" className="taskNameInput" placeholder={props.taskText}></input>
             <img className="taskBtn" src={sliderImg1} id={taskIsChecked ? "taskCompleted" : ""} onClick={taskComplete}/>
 
-            {/* <img className="taskbarSideR" src={sliderImg1} /> */}
         </div>
     )
 };
