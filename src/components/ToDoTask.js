e0a3f703d3a642d5445dfb8e2955d5a8ff5af5d5
kracok <kracok@gmail.com>
2021-03-04 21:01:38 +0100

    styling

--- src/components/ToDoTask.js
+++ src/components/ToDoTask.js
@@ -13,9 +13,9 @@ const Task = (props) => {
 
             <img className="taskPlatform" src={sliderImg1} />
             <img className="taskLogo" src={sliderImg1} />
-            {/* <p className="taskName">{props.taskText}</p> */}
             <input type="text" className="taskNameInput" placeholder={props.taskText}></input>
             <img className="taskBtn" src={sliderImg1} id={taskIsChecked ? "taskCompleted" : ""} onClick={taskComplete}/>
+            {/* <img className="taskLogo" src={sliderImg1} /> */}
 
         </div>
     )
