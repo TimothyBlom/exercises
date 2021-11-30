18b0bc29fdfacd62c264cf6ff095303f93b1437c
kracok <kracok@gmail.com>
2021-11-30 12:15:29 +0100

    added text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,13 @@ const Home = () => {
             <p className="pageHeaderText"> 
                 Coding exercises
             </p>
+
+            <p className="pageParaText">
+                This website has a collection of functions I need to figure out 
+                how to make before I can use it to create my app.
+                <br />
+                Below is an example of how my app is supposed to look like and what it can do when its finished.
+            </p>
                                
         </div>
     )
