1ab59a2168dfb60a6418f9434a1d7e0b9e0aff07
kracok <kracok@gmail.com>
2021-12-06 23:57:03 +0100

    updated header and animations

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,33 +1,68 @@
 import React from 'react';
 import { Link } from "react-browser-router";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, EffectCoverflow } from 'swiper';
+import 'swiper/swiper-bundle.css';
 import headerLogo from "../images/3DLogo.png"
 
 const Header = () => {
     return (
-        <div className='header'>
+        <div className="headerContainer">
 
-            <Link className='routingButtons homeButton' to='/'>            
+            <Link className='homeButton' to='/'>            
                 <img className="headerLogo" src={headerLogo} />
             </Link>
 
-            <Link className='routingButtons' to='/Countdown'>Countdown</Link>
+            <div className="header">
 
-            <Link className='routingButtons' to='/ImgUploader'>Image Uploader</Link>
+                <Swiper className="headerSwiper"
+                    slidesPerView={4}
+                    centeredSlides={true}
+                    navigation
+                    pagination={{ clickable: true }}
+                    pagination
+                    grabCursor={true}
+                    loop={true}
+                >
 
-            <Link className='routingButtons' to='/ApiList'>API Lists</Link>
+                    <SwiperSlide className='routingButtonsContainer'>
+                        <Link className='routingButtons' to='/Countdown'>Countdown</Link>
+                    </SwiperSlide>
 
-            <Link className='routingButtons' to='/SliderList'>Slider lists</Link>
+                    <SwiperSlide className='routingButtonsContainer'>
+                        <Link className='routingButtons' to='/ImgUploader'>Image_Uploader</Link>
+                    </SwiperSlide>
 
-            <Link className='routingButtons' to='/Codex'>Lore Codex</Link>
+                    <SwiperSlide className='routingButtonsContainer'>
+                        <Link className='routingButtons' to='/ApiList'>API_Lists</Link>
+                    </SwiperSlide>
 
-            <Link className='routingButtons' to='/ToDoList1'>Update To Do List</Link>
+                    <SwiperSlide className='routingButtonsContainer'>
+                        <Link className='routingButtons' to='/SliderList'>Slider_lists</Link>
+                    </SwiperSlide>
 
-            <Link className='routingButtons' to='/ToDoList2'>Add taks to To Do List</Link>
+                    <SwiperSlide className='routingButtonsContainer'>
+                        <Link className='routingButtons' to='/Codex'>Lore_Codex</Link>
+                    </SwiperSlide>
 
-            <Link className='routingButtons' to='/ToDoList3'>Simple To Do List</Link>
+                    <SwiperSlide className='routingButtonsContainer'>
+                        <Link className='routingButtons' to='/ToDoList1'>Update_To_Do_List</Link>
+                    </SwiperSlide>
 
-            <Link className='routingButtons' to='/Animations'>Animations</Link>
-                               
+                    <SwiperSlide className='routingButtonsContainer'>
+                        <Link className='routingButtons' to='/ToDoList2'>Add_to_To_Do_List</Link>
+                    </SwiperSlide>
+
+                    <SwiperSlide className='routingButtonsContainer'>
+                        <Link className='routingButtons' to='/ToDoList3'>Simple_To_Do_List</Link>
+                    </SwiperSlide>
+
+                    <SwiperSlide className='routingButtonsContainer'>
+                        <Link className='routingButtons' to='/Animations'>Animations</Link>
+                    </SwiperSlide>
+
+                </Swiper>      
+            </div>
         </div>
     )
 };
--- src/pages/Animations.js
+++ src/pages/Animations.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import wallpaper from "../images/wallpaper1.jpg"
 import logo from "../images/logo1.png"
+import wallpaperUI from "../images/wallpaperUI.png"
 
 const Animations = () => {
-    const [imgIsZoomed, setImgIsZoomed] = useState(false);
+    const [imgIsZoomed1, setImgIsZoomed1] = useState(false);
+    const [imgIsZoomed2, setImgIsZoomed2] = useState(false);
+    const [imgIsZoomed3, setImgIsZoomed3] = useState(false);
 
-    const zoomedImg = () => {
-        setImgIsZoomed(!imgIsZoomed);
+    const zoomedImg1 = () => {
+        setImgIsZoomed1(!imgIsZoomed1);
+    };
+
+    const zoomedImg2 = () => {
+        setImgIsZoomed2(!imgIsZoomed2);
+    };
+
+    const zoomedImg3 = () => {
+        setImgIsZoomed3(!imgIsZoomed3);
     };
 
     return (
@@ -26,17 +37,28 @@ const Animations = () => {
                 <br />
 
                 <p>Image zoom in on click</p>
-                <div className="btnWallpaperContainer" onClick={zoomedImg}>
-                    <img className="imgZoomedOut" src={wallpaper} id={imgIsZoomed ? "imgZoomedIn" : "" } />
+                <div className="btnWallpaperContainer" onClick={zoomedImg1}>
+                    <img className="imgZoomedOut" src={wallpaper} id={imgIsZoomed1 ? "imgZoomedIn" : "" } />
                 </div>
 
                 <br />
 
                 <p>Image zoom in on click with logo and not working shadow, opacity doesn't seem to work on the shadow</p>
-                <div className="btnWallpaperContainer" onClick={zoomedImg}>
-                    <img className="logoZoomedIn" src={logo} id={imgIsZoomed ? "logoZoomedOut" : "" } />
-                    <img className="imgZoomedOut" src={wallpaper} id={imgIsZoomed ? "imgZoomedIn" : "" } />
+                <div className="btnWallpaperContainer" onClick={zoomedImg2}>
+                    <img className="logoZoomedIn" src={logo} id={imgIsZoomed2 ? "logoZoomedOut" : "" } />
+                    <img className="imgZoomedOut" src={wallpaper} id={imgIsZoomed2 ? "imgZoomedIn" : "" } />
                 </div>
+
+                <br />
+
+                <p>Image zoom with border</p>
+                <img className="wallpaperContainerUI" src={wallpaperUI} />
+                <div className="btnWallpaperContainer" onClick={zoomedImg3}>
+                    <img className="logoZoomedIn" src={logo} id={imgIsZoomed3 ? "logoZoomedOut" : "" } />
+                    <img className="imgZoomedOut" src={wallpaper} id={imgIsZoomed3 ? "imgZoomedIn" : "" } />
+                </div>
+
+                <br />
 
             </p>
 
